8e0b58d470782bb514b52942391c57804fd8cc30
janeyrinn <jane.rinn@gmail.com>
2021-04-28 10:04:48 +0000

    Add comments to initial code

--- app.py
+++ app.py
@@ -2,17 +2,17 @@
 import os
 from flask import Flask
 if os.path.exists("env.py"):
-    import env
+    import env 
 
-
+# Creates an instance of flask stored in a variable called "app"
 app = Flask(__name__)
 
-
+# Test function to check app is properly configured
 @app.route("/")
 def hello():
     return "Hello World ... again!"
 
-
+# Tells how and where to run our app 
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
             port=int(os.environ.get("PORT")),
