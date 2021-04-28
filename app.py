860bcb4d31d9f46817e767eb951efa46605f5437
janeyrinn <jane.rinn@gmail.com>
2021-04-28 15:11:44 +0000

    Create base template

--- app.py
+++ app.py
@@ -1,15 +1,32 @@
 import os
-from flask import Flask
+from flask import (
+    Flask, flash, render_template, redirect, request, session, url_for)
+from flask_pymongo import PyMongo
+# Renders bson format from Pymongo
+from bson.objectid import ObjectId
 if os.path.exists("env.py"):
     import env
 
 
+# Creates an instance of the flask app
 app = Flask(__name__)
+
+# App configuration with database
+app.config["MONGO_DBNAME"] = os.environ.get("MONGO_DBNAME")
+app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
+app.secret_key = os.environ.get("SECRET_KEY")
+
+
+# Instance of PyMongo and add the app into in
+mongo = PyMongo(app)
 
 
 @app.route("/")
-def hello():
-    return "Hello World ... again!"
+@app.route("/get_tasks")
+def get_tasks():
+    # will generate date from our tasks collection
+    tasks = mongo.db.tasks.find()
+    return render_template("tasks.html", tasks=tasks)
 
 
 if __name__ == "__main__":
