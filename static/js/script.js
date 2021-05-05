1ea1042da37069d01fe9173aa5a89897ff383b7a
janeyrinn <jane.rinn@gmail.com>
2021-05-05 11:35:40 +0000

    Add comments to CI custom JS

--- static/js/script.js
+++ static/js/script.js
@@ -11,19 +11,25 @@ $(document).ready(function () {
           done: "Select"
       }
   });
+
+  // JS Snippet taken from CI to validate the drop down menu
   validateMaterializeSelect();
   function validateMaterializeSelect() {
+      // custom CSS for valid and invalid
       let classValid = { "border-bottom": "1px solid #4caf50", "box-shadow": "0 1px 0 0 #4caf50" };
       let classInvalid = { "border-bottom": "1px solid #f44336", "box-shadow": "0 1px 0 0 #f44336" };
+      // if statement unhides elements on the DOM
       if ($("select.validate").prop("required")) {
           $("select.validate").css({ "display": "block", "height": "0", "padding": "0", "width": "0", "position": "absolute" });
       }
+      // if one of the items is selected but doesn't have the disabled class accosiated with it
       $(".select-wrapper input.select-dropdown").on("focusin", function () {
           $(this).parent(".select-wrapper").on("change", function () {
               if ($(this).children("ul").children("li.selected:not(.disabled)").on("click", function () { })) {
                   $(this).children("input").css(classValid);
               }
           });
+        // Calls red or green classes depending on user selection
       }).on("click", function () {
           if ($(this).parent(".select-wrapper").children("ul").children("li.selected:not(.disabled)").css("background-color") === "rgba(0, 0, 0, 0.03)") {
               $(this).parent(".select-wrapper").children("input").css(classValid);
